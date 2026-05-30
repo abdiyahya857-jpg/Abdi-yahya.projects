40c81e899f8e6050d766dc7bc76621080fe739c9
abdiyahya857-jpg <abdiyahya857@gmail.com>
2026-05-30 14:17:58 +0300

    Update thought.py

--- thought.py
+++ thought.py
@@ -1,119 +1,37 @@
-solutions = {}
+
+
+  solutions = {}
 
 def evaluate_and_store(op1, op2, op3):
     expression = "4 " + op1 + " 4 " + op2 + " 4 " + op3 + " 4"
     value = eval(expression)
-    
-    if op1 == "//":
-        print_op1 = "/"
-    else:
-        print_op1 = op1
-        
-    if op2 == "//":
-        print_op2 = "/"
-    else:
-        print_op2 = op2
-        
-    if op3 == "//":
-        print_op3 = "/"
-    else:
-        print_op3 = op3
-        
-    display_str = "4 " + print_op1 + " 4 " + print_op2 + " 4 " + print_op3 + " 4 = " + str(value)
+
+    print_op1 = "/" if op1 == "//" else op1
+    print_op2 = "/" if op2 == "//" else op2
+    print_op3 = "/" if op3 == "//" else op3
+
+    display_str = (
+        "4 " + print_op1 +
+        " 4 " + print_op2 +
+        " 4 " + print_op3 +
+        " 4 = " + str(value)
+    )
+
     solutions[value] = display_str
 
-evaluate_and_store("+", "+", "+")
-evaluate_and_store("+", "+", "-")
-evaluate_and_store("+", "+", "*")
-evaluate_and_store("+", "+", "//")
+operators = ["+", "-", "*", "//"]
 
-evaluate_and_store("+", "-", "+")
-evaluate_and_store("+", "-", "-")
-evaluate_and_store("+", "-", "*")
-evaluate_and_store("+", "-", "//")
+for op1 in operators:
+    for op2 in operators:
+        for op3 in operators:
+            evaluate_and_store(op1, op2, op3)
 
-evaluate_and_store("+", "*", "+")
-evaluate_and_store("+", "*", "-")
-evaluate_and_store("+", "*", "*")
-evaluate_and_store("+", "*", "//")
+num_test_cases = int(input())
 
-evaluate_and_store("+", "//", "+")
-evaluate_and_store("+", "//", "-")
-evaluate_and_store("+", "//", "*")
-evaluate_and_store("+", "//", "//")
+for _ in range(num_test_cases):
+    n = int(input())
 
-evaluate_and_store("-", "+", "+")
-evaluate_and_store("-", "+", "-")
-evaluate_and_store("-", "+", "*")
-evaluate_and_store("-", "+", "//")
-
-evaluate_and_store("-", "-", "+")
-evaluate_and_store("-", "-", "-")
-evaluate_and_store("-", "-", "*")
-evaluate_and_store("-", "-", "//")
-
-evaluate_and_store("-", "*", "+")
-evaluate_and_store("-", "*", "-")
-evaluate_and_store("-", "*", "*")
-evaluate_and_store("-", "*", "//")
-
-evaluate_and_store("-", "//", "+")
-evaluate_and_store("-", "//", "-")
-evaluate_and_store("-", "//", "*")
-evaluate_and_store("-", "//", "//")
-
-evaluate_and_store("*", "+", "+")
-evaluate_and_store("*", "+", "-")
-evaluate_and_store("*", "+", "*")
-evaluate_and_store("*", "+", "//")
-
-evaluate_and_store("*", "-", "+")
-evaluate_and_store("*", "-", "-")
-evaluate_and_store("*", "-", "*")
-evaluate_and_store("*", "-", "//")
-
-evaluate_and_store("*", "*", "+")
-evaluate_and_store("*", "*", "-")
-evaluate_and_store("*", "*", "*")
-evaluate_and_store("*", "*", "//")
-
-evaluate_and_store("*", "//", "+")
-evaluate_and_store("*", "//", "-")
-evaluate_and_store("*", "//", "*")
-evaluate_and_store("*", "//", "//")
-
-evaluate_and_store("//", "+", "+")
-evaluate_and_store("//", "+", "-")
-evaluate_and_store("//", "+", "*")
-evaluate_and_store("//", "+", "//")
-
-evaluate_and_store("//", "-", "+")
-evaluate_and_store("//", "-", "-")
-evaluate_and_store("//", "-", "*")
-evaluate_and_store("//", "-", "//")
-
-evaluate_and_store("//", "*", "+")
-evaluate_and_store("//", "*", "-")
-evaluate_and_store("//", "*", "*")
-evaluate_and_store("//", "*", "//")
-
-evaluate_and_store("//", "//", "+")
-evaluate_and_store("//", "//", "-")
-evaluate_and_store("//", "//", "*")
-evaluate_and_store("//", "//", "//")
-
-input_string = input()
-num_test_cases = int(input_string)
-
-current_case = 0
-while current_case < num_test_cases:
-    case_input = input()
-    n = int(case_input)
-    
     if n in solutions:
-        answer = solutions[n]
-        print(answer)
+        print(solutions[n])
     else:
         print("no solution")
-        
-    current_case = current_case + 1
